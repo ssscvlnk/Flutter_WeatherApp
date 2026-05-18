0397cef40e33ab0beb776304a2fbb22d21b6d373
ssscvlnk <pstartceva@yandex.ru>
2026-05-18 21:18:22 +0300

    Add weather model

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,12 @@
+class WeatherModel {
+  final String city;
+  final int temperatue;
+  final String description;
+  final String image;
+  WeatherModel({
+    required this.city,
+    required this.temperatue,
+    required this.description,
+    required this.image
+  });
+}
